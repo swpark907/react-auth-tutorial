c973a7bc118b076f3edef716b7aba7872a3776eb
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-03-08 15:36:09 +0900

    update matchPwd form

--- src/Register.js
+++ src/Register.js
@@ -143,6 +143,35 @@ const Register = () => {
           <span aria-label="percent">%</span>
         </p>
 
+        <label htmlFor="confirm_pwd">
+          Confirm Password:
+          <span className={validMatch && matchPwd ? "valid" : "hide"}>
+            <FontAwesomeIcon icon={faCheck} color="green" />
+          </span>
+          <span className={validMatch || !matchPwd ? "hide" : "invalid"}>
+            <FontAwesomeIcon icon={faTimes} color="red" />
+          </span>
+        </label>
+        <input
+          type="password"
+          id="confirm_pwd"
+          onChange={(e) => {
+            setMatchPwd(e.target.value);
+          }}
+          required
+          aria-invalid={validMatch ? "false" : "true"}
+          aria-descirbedby="confirmnote"
+          onFocus={() => {
+            setMatchFocus(true);
+          }}
+          onBlur={() => {
+            setMatchFocus(false);
+          }}
+        />
+        <p id="confirmnote" className={matchFocus && validMatch ? "instructions" : "offscreen"}>
+          <FontAwesomeIcon icon={faInfoCircle}/>
+          Must match the first password input field.
+        </p>
       </form>
     </section>
   );
